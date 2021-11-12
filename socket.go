b6684d33b7008da5869a398a0f68b6b910bdf920
Mark Smith <mark@theflyingcodr.dev>
2021-11-12 15:37:50 +0000

    removing uneeded nolint

--- socket.go
+++ socket.go
@@ -256,7 +256,6 @@ func isNil(i interface{}) bool {
 	if i == nil {
 		return true
 	}
-	// nolint: exhaustive // don't catering for everything... yet
 	switch reflect.TypeOf(i).Kind() {
 	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
 		return reflect.ValueOf(i).IsNil()
