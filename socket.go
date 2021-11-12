83f22417a7bf1fa594d0fd80dd51045cc8e4148d
Mark Smith <mark@theflyingcodr.dev>
2021-11-12 15:44:20 +0000

    adding it again

--- socket.go
+++ socket.go
@@ -256,6 +256,7 @@ func isNil(i interface{}) bool {
 	if i == nil {
 		return true
 	}
+	// nolint: exhaustive // don't catering for everything... yet
 	switch reflect.TypeOf(i).Kind() {
 	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
 		return reflect.ValueOf(i).IsNil()
